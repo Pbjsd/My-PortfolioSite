c46343be7fc0b089359dd4239366d506a49804c5
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2024-09-30 15:56:14 -0400

    Created About, Experience, Projects, and Contact links in the Navbar section

--- my-portfolio/src/components/Navbar/Navbar.jsx
+++ my-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
 export const Navbar = () => {
-  return 
+  return (
     <nav>
       <a href="/">Portfolio</a>
-    </nav>; 
+      <div>
+        <ul>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#experience">Experience</a>
+            </li>
+            <li>
+                <a href="#projects">Projects</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+      </div>
+    </nav>
+  ); 
 }; 
