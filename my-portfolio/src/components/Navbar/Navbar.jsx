7c5e6e6c4b2deecd4821e66689f43cdbf6b127a7
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2024-10-10 22:44:49 -0400

    Making navbar responsive:  - Adding functionality of menu being able to open and close      - Used useState to create a variable and made this set to false so we can make the navbar close by default
    Making navbar responsive:
    - Adding functionality of menu being able to open and close
        - Used useState to create a variable and made this set to false so we can make the navbar close by default

--- my-portfolio/src/components/Navbar/Navbar.jsx
+++ my-portfolio/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">Portfolio</a>
